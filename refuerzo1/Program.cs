54d811d74e9448c0790e7d584c9b69ef067388db
isaac <isaac@ISAC>
2024-01-21 20:52:02 -0400

    Agregar archivos de proyecto.

---
+++ refuerzo1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Ingrese un número del 0 al 255: ");
+
+string ingresar = Console.ReadLine();
+int numero = int.Parse(ingresar);
+
+Console.WriteLine($"Esta es la tabla del número {numero}: ");
+
+for (int i = 0; i <= 12; i++)
+{
+    int resultado = numero * i;
+    Console.WriteLine($"{i} x {numero} = {resultado}");
+}
+
